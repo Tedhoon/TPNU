e73303b73571bff13a5cb8e5f44f1f97db142019
Tedhoon <gt0305@likelion.org>
2019-09-25 11:12:33 +0900

    base design apply

--- imageboard/forms.py
+++ imageboard/forms.py
@@ -17,4 +17,5 @@ class ImageForm(forms.ModelForm):
                 'title': forms.TextInput(
                     attrs={'class': 'form-control', 'style': 'width: 70%', 'placeholder': '제목을 입력하세요.'}
                 ),
-            }
+            
+        }
