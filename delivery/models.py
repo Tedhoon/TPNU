15d8ccdd52e620daefacec0d684f3c0cb7d0a760
Tedhoon <gt0305@likelion.org>
2019-10-03 14:47:46 +0900

    delivery model

--- delivery/models.py
+++ delivery/models.py
@@ -2,10 +2,27 @@ from django.db import models
 
 class DeliveryList(models.Model):
     title = models.CharField(max_length=100)
-    notion = models.CharField(max_length=300)
+    # deliveryCondition = models.CharField(max_length=300)
     image = models.ImageField(upload_to='image/', blank=True, null=True)
-    menu = models.TextField()
+    
+    # menuname = models.TextField()
+    # menunotion = models.TextField()
+    # menuprice = models.TextField()
+
+
     description = models.CharField(max_length = 150)
+    # deliveryPrice = models.CharField(max_length = 150)
+    # deliveryTime = models.CharField(max_length = 150)
+    # paymentMethod = models.CharField(max_length = 150)
+
+    # restaurant_Info = models.CharField(max_length = 150)
+    # restaurant_OpenTIme = models.CharField(max_length = 150)
+    
+    # **사업자정보**
+    # 상호명
+    # 사업자등록번호
+
+    # 원산지정보
 
     def __str__(self):
         return self.title
