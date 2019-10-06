9879be66d33d911b9c4bbb08c4d5c47446515de7
Tedhoon <gt0305@likelion.org>
2019-10-06 19:07:46 +0900

    딜리버리 모델 수정

--- delivery/models.py
+++ delivery/models.py
@@ -1,28 +1,47 @@
+# from django.db import models
+
+# class DeliveryList(models.Model):
+#     title = models.CharField(max_length=100)
+#     # deliveryCondition = models.CharField(max_length=300)
+#     image = models.ImageField(upload_to='image/', blank=True, null=True)
+    
+#     # menuname = models.TextField()
+#     # menunotion = models.TextField()
+#     # menuprice = models.TextField()
+
+
+#     description = models.CharField(max_length = 150)
+#     # deliveryPrice = models.CharField(max_length = 150)
+#     # deliveryTime = models.CharField(max_length = 150)
+#     # paymentMethod = models.CharField(max_length = 150)
+
+#     # restaurant_Info = models.CharField(max_length = 150)
+#     # restaurant_OpenTIme = models.CharField(max_length = 150)
+    
+#     # **사업자정보**
+#     # 상호명
+#     # 사업자등록번호
+
+#     # 원산지정보
+
+#     def __str__(self):
+#         return self.title
+
+# class DeliveryGroup(models.Model):
+#     pass
+
+# class DeliveryService(models.Model):
+#     pass
+    
+
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class DeliveryList(models.Model):
-    title = models.CharField(max_length=100)
-    # deliveryCondition = models.CharField(max_length=300)
+    title = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to='image/', blank=True, null=True)
-    
-    # menuname = models.TextField()
-    # menunotion = models.TextField()
-    # menuprice = models.TextField()
+    text = RichTextUploadingField(null = True, blank =True)
 
-
-    description = models.CharField(max_length = 150)
-    # deliveryPrice = models.CharField(max_length = 150)
-    # deliveryTime = models.CharField(max_length = 150)
-    # paymentMethod = models.CharField(max_length = 150)
-
-    # restaurant_Info = models.CharField(max_length = 150)
-    # restaurant_OpenTIme = models.CharField(max_length = 150)
-    
-    # **사업자정보**
-    # 상호명
-    # 사업자등록번호
-
-    # 원산지정보
 
     def __str__(self):
         return self.title
